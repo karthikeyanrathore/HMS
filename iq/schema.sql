ae1dfd60520abaf2877a638ee2999b87e6bf6d93
karthikeyanrathore <you@example.com>
2020-11-27 19:41:16 +0530

    sql injection

---
+++ iq/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+	id INTEGER,
+	name TEXT,
+	price INTEGER,
+	stock INTEGER
+);
+
+
+
+
+
+
+
+
---
+++ iq/try.sql
@@ -0,0 +1,5 @@
+SELECT ? FROM ? WHERE  ? = '%pc%';
+
+
+
+
