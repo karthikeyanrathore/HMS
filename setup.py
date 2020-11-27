e655eeca0fedcc0bb26dd1964882819f7cfa9ddf
karthikeyanrathore <you@example.com>
2020-11-27 23:07:34 +0530

    HOST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='iq',
-    version='1.0.0',
+    version='1.1.2',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
