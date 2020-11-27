0c936fdaa5b849c957e4159bb55ea66552229fcd
karthikeyanrathore <you@example.com>
2020-11-27 22:40:18 +0530

    HOST

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='iq',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'wheel',
+    ],
+)
