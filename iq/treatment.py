4184900c01a66c7cd06d990e755a63802c6ec291
karthikeyanrathore <karthikerathore@gmail.com>
2020-12-17 15:46:55 +0530

    delete

--- iq/treatment.py
+++ iq/treatment.py
@@ -33,3 +33,25 @@ def register():
             return redirect(url_for('treatment.home'))
 
     return render_template('treatment/register.html')
+
+@bp.route('/delete', methods=('GET', 'POST'))
+def delete():
+    if request.method == 'POST':
+        T_ID = request.form['T_ID']
+        db = get_db()
+
+        error = None
+        TP = db.execute(
+            'SELECT * FROM Treatment WHERE T_ID = ?', (T_ID,)
+        ).fetchone()
+        if TP is None:
+            error = "Treatment  ID : {} is not registered".format(T_ID)
+        flash(error)
+        if error is None:
+            db.execute('DELETE FROM Treatment WHERE T_ID = ?', (T_ID,))
+            db.commit()
+            return redirect(url_for('treatment.home'))
+
+    return render_template('treatment/delete.html')
+
+
