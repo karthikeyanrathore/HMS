556f6c9244a1b1b87f428b4642d3186e787fd353
karthikeyanrathore <karthikerathore@gmail.com>
2021-05-29 17:07:19 +0530

    rework

--- README.md
+++
@@ -1,5 +0,0 @@
-## Web sql
-### trying sql injection 
-
-
-
--- iq/product.py
+++ iq/product.py
@@ -3,55 +3,50 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from iq.db import get_db
-
 bp = Blueprint('product', __name__, url_prefix='/product')
-
 
 @bp.route('/search', methods=('GET', 'POST'))
 def search():
-    if request.method == 'POST':
-        find = request.form['find']
-        error = None
-        db= get_db()
+  if request.method == 'POST':
+    find = request.form['find']
+    name = request.form['name']
+    error = None
+    db = get_db()
 
-        LP = db.execute(
-                "SELECT * FROM product WHERE id = '%s'" % find).fetchone()
-        #print(LP['stock'])
+    # correct way
+    #LP = db.execute("SELECT * FROM product WHERE id = ? AND name = ?" ,( find , name))
+
+    # wrong way (SQL-injection-attack)
+    #LP = db.execute("SELECT * FROM product")
+    LP = db.execute(f"SELECT * FROM product WHERE id = '{find}' AND name = '{name}'")
     
-        #LP = db.execute(
-                #"SELECT * FROM product WHERE name = " + find).fetchone()
-       
+    if LP  is None:
+      error = "INVALID NAME"
+    return render_template('product/result.html', LP= LP)
 
-        if LP  is None:
-            error = "INVALID NAME"
-        
-        flash(error)
-
-        return render_template('product/result.html', LP= LP)
-
+  if request.method == "GET":
     return render_template('product/search.html')
-
 
 @bp.route('/insert', methods=('GET', 'POST'))
 def insert():
-    error = None
+  error = None
+  if request.method == "POST":
     if error is None:
-        db = get_db()
-        sql = 'INSERT INTO product (id, name , price , stock) VALUES (? , ? , ? , ?)'
-        val = [
-                (1 , 'pen drive' , 200 , 7),
-
-                (2 , 'pc' , 500 , 9),
-                (3 , 'laptop' , 400 , 1),
-                (4 , 'ssd' , 100 , 7),
-                (5 , 'mouse' , 900 , 6),
-                (6 , 'monitor' , 600 , 4)
-                ]
-        db.executemany(sql , val)
-        db.commit()
-        return redirect(url_for('product.search'))
-
+      db = get_db()
+      sql = 'INSERT INTO product (id, name , price , stock) VALUES (? , ? , ? , ?)'
+      val = [
+          (1 , 'pendrive' , 200 , 7),
+          (2 , 'scale' , 500 , 9),
+          (3 , 'laptop' , 400 , 1),
+          (4 , 'ssd' , 100 , 7),
+          (5 , 'mouse' , 900 , 6),
+          (6 , 'monitor' , 600 , 4)
+          ]
+      db.executemany(sql , val)
+      db.commit()
+      return redirect(url_for('product.search'))
+  
+  if request.method == "GET":
     return render_template('product/insert.html')
 
